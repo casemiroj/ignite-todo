3ef6555439540fb5fa78a646d281fe5da83fc1b5
João Casemiro <jv.casemiro@icloud.com>
2022-07-15 16:49:35 -0300

    Estrutura do header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './global.css'
+import styles from './App.module.css'
+import Logo from './assets/logo.svg'
 
 export function App() {
   return(
-    <h1>Hello World</h1>
+    <>
+      <header className={styles.header}>
+        <img src={Logo} alt="Logo to-do" />
+      </header>
+      <div className={styles.input}></div>
+    
+    </>
   )
 }
